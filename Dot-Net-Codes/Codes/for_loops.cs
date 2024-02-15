97da374037fce99308339e7a75ed7b081ee3cf89
prameshwor <prameshworchaudhary14@gmail.com>
2024-02-15 12:07:34 +0545

    change

--- Dot-Net-Codes/Codes/for_loops.cs
+++ Dot-Net-Codes/Codes/for_loops.cs
@@ -8,8 +8,7 @@ namespace ncc_lab_prameshwor
         static void Main(string[] args)
         {
             //sum of natural number
-            int n;
-            int sum = 0;
+            int n, sum = 0;
             Console.Write("Enter the positive interger: ");
             n=int.Parse(Console.ReadLine());
             for(int i=1;i<=n;i++)
