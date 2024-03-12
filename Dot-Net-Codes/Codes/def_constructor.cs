345a877f7587039d9db3823cac0c90a446538e56
prameshwor <prameshworchaudhary14@gmail.com>
2024-03-12 13:19:45 +0545

    default constructor

--- Dot-Net-Codes/Codes/def_constructor.cs
+++ Dot-Net-Codes/Codes/def_constructor.cs
@@ -3,19 +3,22 @@ namespace default_constructor
 {
     class def_const
     {
-        int num;
+        int roll_num;
         string name;
 
         def_const()
         {
-            Console.WriteLine("constructor called ");
+            Console.WriteLine("default constructor called ");
+            Console.WriteLine("Lab no : 1.a");
+            name = "prameshwor chaudhary";
+            roll_num = 17;
         }
 
         public static void Main()
         {
             def_const obj = new def_const();
             Console.WriteLine(obj.name);
-            Console.WriteLine(obj.num);
+            Console.WriteLine(obj.roll_num);
         }
     }
 
--- Dot-Net-Codes/ConsoleApp/Program.cs
+++ Dot-Net-Codes/ConsoleApp/Program.cs
@@ -1,37 +1,25 @@
-﻿//Example of NonGeneric Collection - ArrayList
-using System;
-using System.Collections;
-namespace CollectionApplication
+﻿using System;
+namespace default_constructor
 {
-    class Program
+    class def_const
     {
-        static void Main(string[] args)
+        int roll_num;
+        string name;
+
+        def_const()
         {
-            ArrayList al = new ArrayList();
-            Console.WriteLine("Adding some numbers:");
-            al.Add(45);
-            al.Add(78);
-            al.Add(33);
-            al.Add(56);
-            al.Add(12);
-            al.Add(23);
-            al.Add(9);
-            Console.WriteLine("Capacity: {0} ", al.Capacity);
-            Console.WriteLine("Count: {0}", al.Count);
-            Console.Write("Content: ");
-            foreach (int i in al)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.Write("Sorted Content: ");
-            al.Sort();
-            foreach (int i in al)
-            {
-                Console.Write(i + " ");
-            }
-            Console.WriteLine();
-            Console.ReadKey();
+            Console.WriteLine("default constructor called ");
+            Console.WriteLine("Lab no : 1.a");
+            name = "prameshwor chaudhary";
+            roll_num = 17;
+        }
+
+        public static void Main()
+        {
+            def_const obj = new def_const();
+            Console.WriteLine(obj.name);
+            Console.WriteLine(obj.roll_num);
         }
     }
+
 }
