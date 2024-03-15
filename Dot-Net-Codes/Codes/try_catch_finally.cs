68f4457025aa53ae368a08d89018317f8d6e066e
prameshwor <prameshworchaudhary14@gmail.com>
2024-03-15 12:17:08 +0545

    try catch finally throw

---
+++ Dot-Net-Codes/Codes/try_catch_finally.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace CustomExceptionApp
+{
+    public class InvalidAge : Exception             // Custom Exception
+    {
+        public InvalidAge() { }
+        public InvalidAge(string msg) : base(msg) { }
+    }
+    public class Program
+    {
+        public static void CheckAge(int age)
+        {
+            try
+            {
+                if (age < 16)
+                {   // Deal with 'throw' keyword
+                    throw new InvalidAge("Age should be above 16");
+                }
+                else
+                {
+                    Console.WriteLine("Valied Age is: " + age);
+                }
+            }
+            catch (InvalidAge exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Age Checking Completed!");
+            }
+
+        }
+        public static void Main(string[] args)
+        {
+            int age1 = 15;
+            int age2 = 20;
+
+            CheckAge(age1);
+            CheckAge(age2);
+        }
+    }
+}
--- Dot-Net-Codes/ConsoleApp/Program.cs
+++ Dot-Net-Codes/ConsoleApp/Program.cs
@@ -1,27 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace LambdaDelegate
+namespace LambdaWhere
 {
-    public delegate double MyDelegate(double value);
     public class Program
     {
         public static void Main(string[] args)
         {
-            double num = 5.6d;
-            // Lambda Expression
-            MyDelegate md2 = (x) =>
+            List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
+            // Lambda Expression inside LINQ 'Where()' method used as condition
+            List<int> res = nums.Where(x => x > 2).ToList();
+            foreach (int i in res)
             {
-                Console.Write("Square Value is: ");
-                return x * x;
-            };
-            double square = md2(num);
-            Console.WriteLine(square);
-            Console.WriteLine("\nLab no : 14.a");
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\nLab no : 14.b");
             Console.WriteLine("prameshwor chaudhary");
             Console.WriteLine("roll no : 17");
         }
     }
 }
+
+        
+    
+
 
 
         
