5247bb970b432f6070ab4ba01c430820887a8521
prameshwor <prameshworchaudhary14@gmail.com>
2024-03-15 12:23:01 +0545

    attributes

---
+++ Dot-Net-Codes/Codes/attributes.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace AttributeApp
+{   // Built in Attribute
+    [AttributeUsage(
+    AttributeTargets.Class |
+    AttributeTargets.Struct |
+    AttributeTargets.Constructor |
+    AttributeTargets.Field |
+    AttributeTargets.Property |
+    AttributeTargets.Method,
+    AllowMultiple = true)]
+    public class BugAttribute : Attribute               // Custom Attribute
+    {
+        public string Name { get; set; }
+        public string Developer { get; set; }
+        public string Date { get; set; }
+        public BugAttribute() { }
+        public BugAttribute(string n, string dev)
+        {
+            Name = n;
+            Developer = dev;
+        }
+    }
+    [Bug("Syntax Error", "Alex", Date = "2020-03-5")]
+    [Bug("For Loop Bug", "John", Date = "2020-03-6")]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var attr = Attribute.GetCustomAttributes(typeof(Program));
+            foreach (var a in attr)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
--- Dot-Net-Codes/ConsoleApp/Program.cs
+++ Dot-Net-Codes/ConsoleApp/Program.cs
@@ -1,30 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace LambdaWhere
+namespace CustomExceptionApp
 {
+    public class InvalidAge : Exception             // Custom Exception
+    {
+        public InvalidAge() { }
+        public InvalidAge(string msg) : base(msg) { }
+    }
     public class Program
     {
+        public static void CheckAge(int age)
+        {
+            try
+            {
+                if (age < 16)
+                {   // Deal with 'throw' keyword
+                    throw new InvalidAge("Age should be above 16");
+                }
+                else
+                {
+                    Console.WriteLine("Valied Age is: " + age);
+                }
+            }
+            catch (InvalidAge exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Age Checking Completed!");
+                
+            }
+
+        }
         public static void Main(string[] args)
         {
-            List<int> nums = new List<int>() { 1, 2, 3, 4, 5 };
-            // Lambda Expression inside LINQ 'Where()' method used as condition
-            List<int> res = nums.Where(x => x > 2).ToList();
-            foreach (int i in res)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("\nLab no : 14.b");
+            int age1 = 15;
+            int age2 = 20;
+
+            CheckAge(age1);
+            CheckAge(age2);
+            Console.WriteLine("\n lab no 15");
             Console.WriteLine("prameshwor chaudhary");
             Console.WriteLine("roll no : 17");
         }
     }
 }
-
-        
-    
-
-
-
-        
-    
